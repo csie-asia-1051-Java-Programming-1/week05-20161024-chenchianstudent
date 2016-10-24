60afdefa68657d4b5f6a05d2b37920833d6e4e92
chenchianstudent <105021007@live.asia.edu.tw>
2016-10-24 20:15:36 +0800

    3

--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -7,7 +7,9 @@ public class hw03 {
 //設計一個程式，讓使用者輸入一個正整數n，判斷並輸出n的多少次方才會大於10000
 		Scanner scn=new Scanner(System.in);
 		System.out.print("請輸入n");
-		
+		double n=scn.nextDouble();
+		double n1=Math.ceil(Math.log(10000)/Math.log(n));
+		System.out.println(n1);
 		
 		
 		
